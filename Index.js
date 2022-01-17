a3b2637703d672a8b6e40bab06e9451e40f0781e
Sergey M <sergio007myronenko@gmail.com>
2022-01-17 12:53:26 +0200

    Resolve 1 task

--- Index.js
+++ Index.js
@@ -1,3 +1,14 @@
 "use strict";
 
+//Задание №1//
 
+const width = +prompt('Введите ширину прямоугольника');
+const height = +prompt('Введите высоту прямоугольника');
+
+function area (a, b) {
+    const rectangleArea = a * b;
+    return rectangleArea;
+}
+
+const result = area(width, height);
+alert('Площадь прямоугольника = ' + result);
