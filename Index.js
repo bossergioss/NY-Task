e3c4963683276ae4b23ca03e53b7ed1ec0965441
Sergey M <sergio007myronenko@gmail.com>
2022-01-17 12:56:05 +0200

    Resolve 2 task

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@
 
 //Задание №1//
 
-const width = +prompt('Введите ширину прямоугольника');
+/*const width = +prompt('Введите ширину прямоугольника');
 const height = +prompt('Введите высоту прямоугольника');
 
 function area (a, b) {
@@ -11,4 +11,18 @@ function area (a, b) {
 }
 
 const result = area(width, height);
-alert('Площадь прямоугольника = ' + result);
+alert('Площадь прямоугольника = ' + result);*/
+
+//Задание №2//
+
+const PI = 3.14;
+
+const diameter = Number(prompt('Введите диаметр окружности'));
+
+function length (x) {
+    const lengcirc = PI * x;
+    return lengcirc;
+}
+
+const result = length(diameter);
+alert('Длина окружности = ' + result);
